f4c681b5111fc474dbf14bdf378382649f8d625a
toannq06031999 <toannq06031999@gmail.com>
2019-08-16 14:04:20 +0700

    s

---
+++ EndToEnd/Migrations/201908150056516_InitialCreate1.cs
@@ -0,0 +1,16 @@
+namespace EndToEnd.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ EndToEnd/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EndToEnd.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
